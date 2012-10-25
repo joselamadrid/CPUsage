69bb83e73c2928716891926d9fb6460a156730e6
Joselamadrid <sysadminktp@mail.ru>
2012-10-25 15:42:26 +0400

    Cpu usage evaluation code added

--- CPUsage/AppDelegate.h
+++ CPUsage/AppDelegate.h
@@ -7,8 +7,21 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
+#include <mach/mach.h>
+#include <mach/processor_info.h>
+#include <mach/mach_host.h>
+
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+	processor_info_array_t cpuInfo, prevCpuInfo;
+	mach_msg_type_number_t numCpuInfo, numPrevCpuInfo;
+	unsigned numCPUs;
+	NSTimer *updateTimer;
+	NSLock *CPUUsageLock;
+}
 
 @property (assign) IBOutlet NSWindow *window;
 
